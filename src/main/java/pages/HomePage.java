151e5b64d7aed6bb218cbb496cfe5dc8cfd72125
Stas <stanyslaw25@gmail.com>
2023-12-14 00:20:34 +0200

    PagesUpdate

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -2,10 +2,14 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class HomePage {
     WebDriver driver;
+    WebDriverWait wait;
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
@@ -15,6 +19,7 @@ public class HomePage {
 //    List<WebElement> elements = (List<WebElement>) By.cssSelector("div[class='card mt-4 top-card']");
 
     public FunctionsPage clickOnElements(){
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(element)));
         driver.findElement(element).click();
         return new FunctionsPage(driver);
     }
